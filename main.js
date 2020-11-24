8efc920e3dabddd6e39d853893e4bb858b554441
unknown <Jhoandavidcontreras14@gmail.com>
2020-11-24 11:53:22 -0500

    jquery23-112020

---
+++ main.js
@@ -0,0 +1,36 @@
+let nombre="Jhoan David";
+let apellido="Contreras Cardenas";
+let edad="18";
+let genero="masculino";
+let camisa="llevo camisa verde";
+let pies_lleva_calsado = false;
+let color_del_pelo="Negro";
+let tipo_de_pelo="ondulado";
+let color_de_ojos="miel";
+let color_de_piel="blanco";
+let altura="1.88";
+let hobbie="Fotografia"
+let serie_favorita="Riverdale"
+let genero_de_musica_favorito="pop"
+let actor_favorito="Cole Sprouse"
+let cantante__favorito="Shawn Mendes"
+let comida_favorita="Pizza"
+let red_social_favotita="instagram"
+console.log("Mi nonbre es:"+ nombre  )
+console.log("Mi apellido es: "+apellido )
+console.log("edad:"+ edad)
+console.log("genero:"+genero)
+console.log("camisa:"+camisa)
+console.log("pies lleva calsado:"+pies_lleva_calsado )
+console.log("color de pelo:"+color_del_pelo)
+console.log("tipo de pelo:"+ tipo_de_pelo)
+console.log("color de ojos:"+color_del_pelo)
+console.log("color de piel:"+color_de_piel)
+console.log("altura:"+altura)
+console.log("hobbie:"+hobbie)
+console.log("serie favorita:"+serie_favorita)
+console.log("genero de musica favorito:"+genero_de_musica_favorito)
+console.log("actor favorita:"+actor_favorito)
+console.log("cantante favorito:"+cantante__favorito)
+console.log("comida favorita:"+comida_favorita)
+console.log("red social favorita:"+red_social_favotita)
